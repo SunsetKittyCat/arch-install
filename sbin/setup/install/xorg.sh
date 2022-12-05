794d1969f53953dedad6135b3bc34db2c0917ee3
SunsetKittyCat <bogdanvriend@gmail.com>
2022-12-04 19:43:07 -0500

    Fixed Quote Issues In xorg Config File Setup

--- sbin/setup/install/xorg.sh
+++ sbin/setup/install/xorg.sh
@@ -7,12 +7,12 @@ GPU_VENDORS=$(tr '[:upper:]' '[:lower:]' <<< $(lspci -v | grep -A1 -e VGA -e 3D)
 # If AMD GPU Is Present, Install xf86-video-amdgpu mesa vulkan-radeon libva-mesa-driver mesa-vdpau For AMD GPU Acceleration And Vulkan Support
 if [[ $GPU_VENDORS =~ "amd" ]]; then
     pacman --noconfirm --needed -S xf86-video-amdgpu mesa vulkan-radeon libva-mesa-driver mesa-vdpau
-    echo -e "Section "Device"\n     Identifier "AMD"\n     Driver "amdgpu"\n     Option "TearFree" "true"\n     Option "DRI" "3"\n     Option "VariableRefresh" "true"\nEndSection" > /etc/X11/xorg.conf.d/20-amdgpu.conf
+    echo -e 'Section "Device"\n     Identifier "AMD"\n     Driver "amdgpu"\n     Option "TearFree" "true"\n     Option "DRI" "3"\n     Option "VariableRefresh" "true"\nEndSection' > /etc/X11/xorg.conf.d/20-amdgpu.conf
 fi
 # If Intel GPU Is Present, Install xf86-video-intel mesa vulkan-intel intel-media-driver For Intel GPU Acceleration And Vulkan Support
 if [[ $GPU_VENDORS =~ "intel" ]]; then
     pacman --noconfirm --needed -S xf86-video-intel mesa vulkan-intel intel-media-driver
-    echo -e "Section "Device"\n     Identifier "Intel Graphics"\n     Driver "intel"\n	 Option "AccelMethod" "sna"\n	 Option "TearFree" "true"\nEndSection" > /etc/X11/xorg.conf.d/20-intel.conf
+    echo -e 'Section "Device"\n     Identifier "Intel Graphics"\n     Driver "intel"\n	 Option "AccelMethod" "sna"\n	 Option "TearFree" "true"\nEndSection' > /etc/X11/xorg.conf.d/20-intel.conf
 fi
 # If NVidia GPU Is Present, Install nvidia nvidia-utils For NVidia GPU Acceleration And Vulkan Support
 if [[ $GPU_VENDORS =~ "nvidia" ]]; then
